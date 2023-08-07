bcb5e5868762dc8e1abf432e50f631a0c10be519
shriharijadhav <mr.shrihari212@gmail.com>
2023-08-07 13:06:16 +0530

    code changes

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import search from '../images/Search.svg'
- import SearchIcon from './SearchIcon';
+import SearchIcon from './SearchIcon';
 import { topLevelContext } from '../Context';
 
 const HeaderBar = () => {
@@ -11,8 +11,8 @@ const HeaderBar = () => {
   return (
     <div className='pt-2 flex justify-center items-center z-10'>
    
-        <form onSubmit={enqueue}  className='flex'>
-            <input type='text' name='' id='searchbar' required className='relative shadow-md h-12 bg-grey-50 w-72 z-10 rounded-l-xl pl-5 pr-2 bg-gray-100 dark:dark:bg-gray-900   outline-none  text-gray-900 dark:text-gray-300' placeholder='Enter Youtube Video/Playlist URL'
+        <form onSubmit={enqueue}  className='flex w-full px-2'>
+            <input type='text' name='' id='searchbar' required className='relative shadow-md h-12 bg-grey-50 w-full z-10 rounded-l-xl pl-3  bg-gray-100 dark:dark:bg-gray-900   outline-none  text-gray-900 dark:text-gray-300' placeholder='Enter Youtube Video/Playlist URL & click +'
             value={inputUrl} onChange={handleChange} />
             <button type='submit'  className='inline-block px-4 h-12 -ml-1 mr-2 shadow-md text-center text-white uppercase transition duration-200 ease-in-out bg-indigo-500 rounded-r-xl cursor-pointer outline-none hover:bg-indigo-600  font-semibold' title='Add Song/Playlist'>+
             </button>
