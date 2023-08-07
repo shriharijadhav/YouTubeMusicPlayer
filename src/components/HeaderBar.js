fd8c6ca2a5359fb217e86ec7cf7eb8b32320e2e0
shriharijadhav <mr.shrihari212@gmail.com>
2023-08-07 13:17:06 +0530

    add button margin changes

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -12,9 +12,9 @@ const HeaderBar = () => {
     <div className='pt-2 flex justify-center items-center z-10'>
    
         <form onSubmit={enqueue}  className='flex w-full px-2'>
-            <input type='text' name='' id='searchbar' required className='relative shadow-md h-12 bg-grey-50 w-full z-10 rounded-l-xl pl-3  bg-gray-100 dark:dark:bg-gray-900   outline-none  text-gray-900 dark:text-gray-300' placeholder='Enter Youtube Video/Playlist URL & click +'
+            <input type='text' name='' id='searchbar' required className='relative shadow-md h-12 bg-grey-50 w-full z-10 rounded-l-xl pl-3  bg-gray-100 dark:dark:bg-gray-900   outline-none  text-gray-900 dark:text-gray-300' placeholder='Enter Youtube Video/Playlist URL & click (+)'
             value={inputUrl} onChange={handleChange} />
-            <button type='submit'  className='inline-block px-4 h-12 -ml-1 mr-2 shadow-md text-center text-white uppercase transition duration-200 ease-in-out bg-indigo-500 rounded-r-xl cursor-pointer outline-none hover:bg-indigo-600  font-semibold' title='Add Song/Playlist'>+
+            <button type='submit'  className='inline-block px-4 h-12 -ml-1 mr-0 shadow-md text-center text-white uppercase transition duration-200 ease-in-out bg-indigo-500 rounded-r-xl cursor-pointer outline-none hover:bg-indigo-600  font-semibold' title='Add Song/Playlist'>+
             </button>
     </form>
 </div>
