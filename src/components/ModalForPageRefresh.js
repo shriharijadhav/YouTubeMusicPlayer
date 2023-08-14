2377c7ebba6739c9569923c7361d4b0fde3a437b
shriharijadhav <mr.shrihari212@gmail.com>
2023-08-14 17:03:26 +0530

    code changes -minor

--- src/components/ModalForPageRefresh.js
+++ src/components/ModalForPageRefresh.js
@@ -10,7 +10,7 @@ function ModalForPageRefresh() {
       <div className='modalContainer  dark:bg-gray-400 bg-indigo-300 dark:text-indigo-900 text-indigo-900 w-10/12 sm:w-10/12 md:w-7/12 lg:w-7/12 xl:w-7/12 mx-auto flex flex-col gap-5 z-50 p-10 rounded-xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
     
       <div className='flex flex-row  w-full justify-end items-center border-b-2 p-3 '>
-          <div className='flex flex-row w-full justify-between  items-center text-xl dark:text-indigo-900 md:px-2'>
+          <div className='flex flex-row w-full justify-between  items-center text-xl dark:text-indigo-900 md:px-0'>
           <div className='text-xl font-bold'>Page Refresh Manager</div>
           <div className='text-xl cursor-pointer'onClick={()=>setModalForPageRefresh(false)} title='close'>X</div>
         </div>
