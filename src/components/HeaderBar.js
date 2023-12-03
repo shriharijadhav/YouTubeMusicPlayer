d80142fcaaa6c7c769f09dea1201a88849a4ac7a
shriharijadhav <mr.shrihari212@gmail.com>
2023-12-03 14:36:04 +0530

    minor code changes

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -9,7 +9,7 @@ const HeaderBar = () => {
  const {inputUrl,handleChange, enqueue} = useContext(topLevelContext);
 
   return (
-    <div className='pt-2 flex justify-center items-center z-50'>
+    <div className='pt-2 flex justify-center items-center -z-10'>
    
         <form onSubmit={enqueue}  className='flex w-full px-2 '>
             <input type='text' name='' id='searchbar' required className='relative shadow-md h-12 bg-grey-50 w-full z-10 rounded-l-xl pl-3  bg-gray-100 dark:dark:bg-gray-900   outline-none  text-gray-900 dark:text-gray-300' placeholder='Enter Youtube Video/Playlist URL & click (+)'
